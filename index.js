95b7b19aae6794953f7efe6ebb7dcc6d37fba621
Junsu <pksjmh5295@gmail.com>
2023-02-17 15:42:02 +0900

    day18 - 5th

--- index.js
+++ index.js
@@ -1,78 +1,107 @@
 const http = require("http")
 const express = require("express")
-const cors = require("cors")
-const dotenv = require("dotenv")
-const { Router } = require("express")
 const app = express()
+const cors = require("cors")
+const router = express.Router()
 
-const router = Router()
+const cookieParser = require("cookie-parser")
+const expressSession = require("express-session")
 
-dotenv.config({ path: "env/.env.local" })
-
-app.set("views", __dirname + "/views")
-app.set("view engine", "ejs")
-
+// ejs 뷰엔진을 사용하기 위한 설정 - 뷰템플릿
+app.set("views", __dirname + "/views") // prefix - 폴더 지정
+app.set("view engine", "ejs") // suffix - 확장자
+process.env.PORT = 3003
 app.set("port", process.env.PORT || 3001)
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// cookieParser 미들웨어 설정
+app.use(cookieParser())
+// session 미들웨어 설정
+app.use(
+    expressSession({
+        secret: "my key",
+        resave: true,
+        saveUninitialized: true,
+    })
+)
 
+// cors 미들웨어 설정
+app.use(cors())
+// bodyParser 미들웨어 설정 부분.
+app.use(express.json()) // application/json
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+// static 미들웨어 설정 - express에 내장.
 app.use(express.static(__dirname + "/public"))
 
-// 라우터 객체에 넣어줌
+/////// router
+router.route("/home").get((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf8" })
+    res.write("<h1>길동이의 홈페이지</h1>")
+    res.end()
+})
+
 router.route("/login").post((req, res) => {
-    // 로그인 처리
-    const { id, passwd } = req.body
-    console.log(id, passwd)
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf8;" })
+    console.log("POST - /login")
+    console.log(req.body)
 
+    if (req.session.user) {
+        // 이미 로그인 되어있음
+        console.log("Already logged in")
+    } else {
+        // 로그인 진행
+        req.session.user = {
+            id: req.body.id,
+            name: "박준수",
+            authorized: true,
+        }
+    }
+
+    res.write(`<h1>${req.session.user.name} 님 환영합니다.</h1>`)
+    res.write(`<a href="/process/product" >상품 페이지 이동</a>`)
+    res.end()
+})
+
+router.route("/process/product").post((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf8;" })
+
+    if (req.session.user) {
+        // session에 user 정보가 있음 (로그인 되어있는 상태)
+        res.end("<h1>제품 상세 정보</h1>")
+    } else {
+        // 로그아웃된 상태
+        res.redirect("/html/loginForm.html")
+    }
+})
+
+router.route("/login/cookie").post((req, res) => {
+    console.log("POST - /login")
+    console.log(req.body)
+    // 쿠키는 사용자 쪽 (Client)에 생성된다. res에 저장.
+    // npm i cookie-parser -S
+    // const cookieParser = require("cookie-parser");
+    // app.use(cookieParser());
+    res.cookie("user", {
+        id: req.body.id,
+        password: req.body.passwd,
+        name: "박준수",
+        authorized: true,
+    })
     res.redirect("/")
-})
-
-// router로 REST 방식의 요청 처리 테스트
-// get(), put(), post(), delete()
-router.route("/board").get((req, res) => {
-    // 로그인 처리
-
-    console.log("GET")
-
-    res.redirect("/")
-})
-router.route("/board").post((req, res) => {
-    console.log("POST")
-
-    res.redirect("/")
-})
-router.route("/board/:user/:job").post((req, res) => {
-    console.log("POST - /board/:user/:job")
-    const { user, job } = req.params
-    console.log(user, job)
-
-    res.send({ data: req.params })
-})
-
-router.route("/board").put((req, res) => {
-    console.log("PUT")
-
-    res.send({ data: req.body })
-})
-router.route("/board").delete((req, res) => {
-    console.log("DELETE")
-
-    res.send({ data: req.body })
 })
 
 app.use("/", router)
-
-// error 처리 - 라우터 미들웨어 아래에 위치해야 함
-app.all("*", (req, res) => {
-    // res.status(400).end("404 NOT FOUND")
-    res.redirect("./404.html")
-    // res.status(404).redirect("./404.html")
+//오류 핸들러 모듈 사용
+var expressErrorHandler = require("express-error-handler")
+//모든 라우터 처리 후 404 오류 페이지 처리
+var errorHandler = expressErrorHandler({
+    static: {
+        404: "./public/404.html",
+    },
 })
-
-// express-error-handler 사용
-
+app.use(expressErrorHandler.httpError(404))
+app.use(errorHandler)
+// 서버 실행
 const server = http.createServer(app)
 server.listen(app.get("port"), () => {
-    console.log(`Nodejs Server running... port ${app.get("port")}`)
+    console.log("Node.js 서버 실행 중 ... http://localhost:" + app.get("port"))
 })
